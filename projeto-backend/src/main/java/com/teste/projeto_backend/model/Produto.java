42d3afe37d6994db1379ed7cd898aaba0e98fd93
Adriel <adrielbinda@gmail.com>
2025-04-11 22:20:02 -0300

    Realizando as primeiras configurações para o projeto

---
+++ projeto-backend/src/main/java/com/teste/projeto_backend/model/Produto.java
@@ -0,0 +1,15 @@
+package com.teste.projeto_backend.model;
+
+public class Produto {
+
+    private Integer id;
+
+    private String nome;
+
+    private Integer quantidade;
+
+    private Double valor;
+    
+    private String observacao;
+    
+}
