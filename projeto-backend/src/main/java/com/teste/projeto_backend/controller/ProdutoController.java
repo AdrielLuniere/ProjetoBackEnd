e219870f26ae01b54c166b34dfde1edf968640f9
Adriel <adrielbinda@gmail.com>
2025-09-05 03:53:50 -0300

    Preparando o controller

--- projeto-backend/src/main/java/com/teste/projeto_backend/controller/ProdutoController.java
+++ projeto-backend/src/main/java/com/teste/projeto_backend/controller/ProdutoController.java
@@ -1,8 +1,17 @@
 package com.teste.projeto_backend.controller;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@restController 
-
+@RestController 
+@RequestMapping("/api/produtos")
 public class ProdutoController {
+
+    @Autowired 
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> obterTodos() {
+        return produtoService.obterTodos();
+    }
     
 }
